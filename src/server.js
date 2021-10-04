e770453b50ec9070f01c2a5b894f30523684b69e
Anas Syaifudin <anzt.07@gmail.com>
2021-10-05 04:19:26 +0700

    menambahkan authentikasi

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 // songs
 const songs = require('./api/songs');
@@ -13,10 +14,17 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
-  
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -26,11 +34,10 @@ const init = async () => {
       },
     },
   });
-  
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
-
 
     if (response instanceof ClientError) {
       // membuat response baru dari response toolkit sesuai kebutuhan error handling
@@ -42,7 +49,28 @@ const init = async () => {
       return newResponse;
     }
     return response.continue || response;
+  });
 
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('songsapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -60,8 +88,16 @@ const init = async () => {
         validator: UsersValidator,
       },
     },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
   ]);
-
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -1,74 +1,77 @@
-const { nanoid } = require("nanoid");
-const { Pool } = require("pg");
-const InvariantError = require("../../exceptions/InvariantError");
-const NotFoundError = require("../../exceptions/NotFoundError");        
-const { mapDBToModel } = require("../../utils");
+/* eslint-disable no-underscore-dangle */
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const { mapDBToModel } = require('../../utils');
 
 class SongsService {
-    constructor() {
-        this._pool = new Pool();
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong(payload) {
+    const id = `song-${nanoid(16)}`;
+    const insertedAt = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $7) RETURNING id',
+      values: [id, ...Object.values(payload), insertedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
     }
+    return result.rows[0].id;
+  }
 
-    async addSong(payload) {
-        const id = `song-${nanoid(16)}`;
-        const insertedAt = new Date().toISOString();
+  async getSongs() {
+    const { rows } = await this._pool.query('SELECT id, title, performer FROM songs');
+    return rows.map(mapDBToModel);
+  }
 
-        const query = {
-            text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $7) RETURNING id',
-            values: [id, ...Object.values(payload), insertedAt],
-        };
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
 
-        const result = await this._pool.query(query);
-
-        if (!result.rows[0].id) {
-            throw new InvariantError('Lagu gagal ditambahkan');
-        }
-        return result.rows[0].id;
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan');
     }
+    return result.rows.map(mapDBToModel)[0];
+  }
 
-    async getSongs() {
-        const { rows } = await this._pool.query('SELECT id, title, performer FROM songs');
-        return rows.map(mapDBToModel);
+  async editSongById(songId, {
+    title, year, performer, genre, duration,
+  }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, performer = $3, genre = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id',
+      values: [title, year, performer, genre, duration, updatedAt, songId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
     }
+  }
 
-    async getSongById(id) {
-        const query = {
-            text: 'SELECT * FROM songs WHERE id = $1',
-            values: [id],
-        };
-        const result = await this._pool.query(query);
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
 
-        if (!result.rowCount) {
-            throw new NotFoundError('Lagu tidak ditemukan');
-        }
-        return result.rows.map(mapDBToModel)[0];
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
     }
-
-    async editSongById(songId, { title, year, performer, genre, duration }) {
-        const updatedAt = new Date().toISOString();
-        const query = {
-            text: 'UPDATE songs SET title = $1, year = $2, performer = $3, genre = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id',
-            values: [title, year, performer, genre, duration, updatedAt, songId],
-        };
-
-        const result = await this._pool.query(query);
-
-        if (!result.rows.length) {
-            throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
-        }
-    }
-
-    async deleteSongById(id) {
-        const query = {
-            text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
-            values: [id],
-        };
-
-        const result = await this._pool.query(query);
-
-        if (!result.rows.length) {
-            throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
-        }
-    }
+  }
 }
 module.exports = SongsService;
